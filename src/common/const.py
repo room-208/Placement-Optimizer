fede7d7d3943b166fea6a15ada9035445c1e4d4f
room-208 <yumalegomindstorm@gmail.com>
2024-06-03 23:43:18 +0900

    fix：パラメータを修正しました

--- src/common/const.py
+++ src/common/const.py
@@ -8,12 +8,11 @@ DATA_DIR = ROOT_DIR / "data"
 SRC_DIR = ROOT_DIR / "src"
 OUTPUTS_DIR = ROOT_DIR / "outputs"
 
-PARAMS_JSON_PATH = DATA_DIR / "params.json"
 LOTS_CSV_PATH = DATA_DIR / "lots.csv"
 YARDS_CSV_PATH = DATA_DIR / "yards.csv"
 
-ASSIGNMENTS_CSV_PATH = (
-    lambda stage: OUTPUTS_DIR / f"assignments_stage_{str(stage).zfill(4)}.csv"
+PLACEMENTS_CSV_PATH = (
+    lambda stage: OUTPUTS_DIR / f"placements_stage_{str(stage).zfill(4)}.csv"
 )
 ANIMATION_GIT_PATH = OUTPUTS_DIR / "animation.gif"
 
@@ -22,8 +21,5 @@ N = 60
 M = 2
 H = 50
 W = 20
-T = 20
-
-PENALTY_ALPHA = 2
 
 NUM_COLS = 2
