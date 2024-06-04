752dcf109e782660486bb00ca205099b50fddb50
room-208 <yumalegomindstorm@gmail.com>
2024-06-04 12:36:18 +0900

    fix：修正しました

--- src/visualizer/reader.py
+++ src/visualizer/reader.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-from common.const import LOTS_CSV_PATH, PLACEMENTS_CSV_PATH
+from common.const import PLACEMENTS_CSV_PATH
 
 
-def make_merged_placements_df(stage: int) -> tuple[pd.DataFrame, pd.DataFrame]:
-    lots_df = pd.read_csv(LOTS_CSV_PATH)
+def make_placements_df(stage: int) -> tuple[pd.DataFrame, pd.DataFrame]:
     placements_df = pd.read_csv(PLACEMENTS_CSV_PATH(stage))
-    merged_df = pd.merge(lots_df, placements_df, left_index=True, right_index=True)
-    can_placements_df = merged_df[~merged_df["x"].isnull()].reset_index(drop=True)
-    cannot_placements_df = merged_df[merged_df["x"].isnull()].reset_index(drop=True)
+    can_placements_df = placements_df[~placements_df["x"].isnull()].reset_index(
+        drop=True
+    )
+    cannot_placements_df = placements_df[placements_df["x"].isnull()].reset_index(
+        drop=True
+    )
     return can_placements_df, cannot_placements_df
